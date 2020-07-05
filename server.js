82e6e9104b8befba50fc23296cb08496cd8cf05c
Leonel Aimar <leoaimar22@gmail.com>
2020-07-05 20:54:00 -0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if ( process.env.NODE_ENV !== 'production' ) {
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -17,15 +18,24 @@ db.once('open', () => console.log('Connected to Mongoose'))
 const app = express();
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 
+app.use(bodyParser.urlencoded({ 
+    limit: '10mb', 
+    extended: false 
+    })
+);
 app.use(expressLayouts);
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server listening on port 3000')
