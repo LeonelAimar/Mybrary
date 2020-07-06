2910b035b11be4e712c5cf541e1359226a85948a
Leonel Aimar <leoaimar22@gmail.com>
2020-07-06 17:40:58 -0300

    Changed from localStorage img, to send them to DB

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
+// const multer = require('multer')
 const Book = require('../models/book')
 const Author = require('../models/author')
 const path = require('path')
 const fs = require('fs')
-const uploadPath = path.join('public', Book.coverImageBasePath)
+// const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
+// const upload = multer({
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) => {
+//         callback(null, imageMimeTypes.includes(file.mimetype))
+//     }
+// })
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -44,16 +44,18 @@ router.get('/new', async (req, res) => {
 })
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+// router.post('/', upload.single('cover'), async (req, res) => {    <----- this is for only upload the multipart filename of file to db without filepond
+router.post('/', async (req, res) => {
+    // const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
+        // coverImageName: fileName,
         description: req.body.description
     })
+    saveCover(book, req.body.cover)
 
     try {
         const newBook = await book.save()
@@ -61,7 +63,7 @@ router.post('/', upload.single('cover'), async (req, res) => {
         res.redirect(`books`)
     } catch (err){
         if ( book.coverImageName != null ) {
-            removeBookCover(book.coverImageName)
+            // removeBookCover(book.coverImageName)
         }
         renderNewPage(res, book, true, err)
     }
@@ -69,11 +71,11 @@ router.post('/', upload.single('cover'), async (req, res) => {
 
 
 // Functions
-function removeBookCover( coverName ) {
-    fs.unlink(path.join(uploadPath, coverName), err => {
-        if (err) console.error(err)
-    })
-}
+// function removeBookCover( coverName ) {
+//     fs.unlink(path.join(uploadPath, coverName), err => {
+//         if (err) console.error(err)
+//     })
+// }
 
 async function renderNewPage(res, book, hasError = false, error = null){
     try {
@@ -87,6 +89,15 @@ async function renderNewPage(res, book, hasError = false, error = null){
         res.render('books/new', params)
     } catch {
         res.redirect('/books')
+    }
+}
+
+function saveCover(book, coverEncoded) {
+    if ( coverEncoded == null ) return
+    const cover = JSON.parse(coverEncoded)
+    if ( cover != null && imageMimeTypes.includes(cover.type) ) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
     }
 }
 
